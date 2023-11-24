dfb2031e1d3b21fa3a4ce9ed1058f8fb79ec1e45
Kavish-Desai <147175708+Kavish-Desai@users.noreply.github.com>
2023-11-24 14:58:24 -0500

    Update ex5.py

--- src/ex5.py
+++ src/ex5.py
@@ -1,7 +1,26 @@
 from pprint import pprint
 
 def build_car_list():
-    pass # TODO
+    car_list = []
+
+    # Read valid car IDs and models from car-ids.txt file
+    car_ids_and_models = {}
+    with open('C:\\Users\\f9xuyp3\python-3\\files\\car-ids.txt', 'r') as ids_file:
+        for line in ids_file:
+            car_id, model = line.strip().split(', ')
+            car_ids_and_models[int(car_id)] = model
+
+    # Read car details from input.txt and filter out outliers
+    with open('C:\\Users\\f9xuyp3\python-3\\files\\input.txt', 'r') as input_file:
+        next(input_file)  # Skip the header line
+        for line in input_file:
+            car_id, mileage = line.strip().split(', ')
+            car_id = int(car_id)
+            mileage = float(mileage)
+            if car_id in car_ids_and_models and mileage.is_integer():
+                car_list.append({'id': car_id, 'miles': int(mileage), 'model': car_ids_and_models[car_id]})
+
+    return car_list
 
 
 def ex5():
