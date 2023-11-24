f6654b95fdb7f400634c033a1a45402dde619ae0
Kavish-Desai <147175708+Kavish-Desai@users.noreply.github.com>
2023-11-24 14:57:34 -0500

    Update ex3.py

--- src/ex3.py
+++ src/ex3.py
@@ -1,11 +1,38 @@
 
-def create_files():
-    pass # TODO
+def create_files(input_file):
+    small_words = []
+    large_words = []
+    unique_words = set()
+
+    try:
+        with open(input_file, 'r') as file:
+            text = file.read().split()
+            unique_words = set(text)
+
+            for word in unique_words:
+                if len(word) < 3:
+                    small_words.append(word)
+                else:
+                    large_words.append(word)
+
+        with open('small-words.txt', 'w') as small_file:
+            for word in small_words:
+                small_file.write(word + '\n')
+
+        with open('large-words.txt', 'w') as large_file:
+            for word in large_words:
+                large_file.write(word + '\n')
+
+        return len(unique_words)
+    except FileNotFoundError:
+        print(f"File '{input_file}' not found.")
+        return 0
+    
 
 
 
 def ex3():
-    total_words = create_files("words.txt")
+    total_words = create_files("C:\\Users\\f9xuyp3\python-3\\files\\words.txt")
     print(f"Total words: {total_words}.")
 
 ex3()
